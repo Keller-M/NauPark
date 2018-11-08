742316591703df287993460147fb214afc446a75
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-07 22:44:01 -0700

    created class, added description

---
+++ app/src/main/java/com/code/monkey/naupark/ParkingLot.java
@@ -0,0 +1,11 @@
+package com.code.monkey.naupark;
+
+/**
+ * Encapsulates information needed to define a ParkingLot.
+ * A parking lot has a list of passes that are allowed to park in it,
+ * as well as information about what times it becomes freely available,
+ * e.g. after 4:30PM on weekdays, or at any time during the weekends.
+ */
+abstract public class ParkingLot
+{
+}
