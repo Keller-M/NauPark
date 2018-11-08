dee547a9d5761445b2b521a36b940397b0c0b24e
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-07 22:54:57 -0700

    changed ParkingLot documentation

--- app/src/main/java/com/code/monkey/naupark/ParkingLot.java
+++ app/src/main/java/com/code/monkey/naupark/ParkingLot.java
@@ -2,10 +2,11 @@ package com.code.monkey.naupark;
 
 /**
  * Encapsulates information needed to define a ParkingLot.
- * A parking lot has a list of passes that are allowed to park in it,
- * as well as information about what times it becomes freely available,
+ * A parking lot has informatino about what times it becomes freely
+ * available.
  * e.g. after 4:30PM on weekdays, or at any time during the weekends.
  */
 abstract public class ParkingLot
 {
+
 }
