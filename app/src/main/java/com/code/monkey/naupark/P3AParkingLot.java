827275fb52d4a4ba4352b3990f30615e383acaf0
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:03 -0700

    created P3AParkingLot.java

---
+++ app/src/main/java/com/code/monkey/naupark/P3AParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P3AParkingLot extends ParkingLot 
+ { 
+
+    public P3AParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
