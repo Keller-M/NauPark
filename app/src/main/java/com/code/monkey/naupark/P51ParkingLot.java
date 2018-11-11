b68f3ed01849e06050b6badef85bd3fa7f61b6f4
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:02 -0700

    created P51ParkingLot.java

--- app/src/main/java/com/code/monkey/naupark/P51ParkingLot.java
+++ app/src/main/java/com/code/monkey/naupark/P51ParkingLot.java
@@ -8,3 +8,13 @@ public class P51ParkingLot extends ParkingLot
         super( mode, name );
     }
 }
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P51ParkingLot extends ParkingLot 
+ { 
+
+    public P51ParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
