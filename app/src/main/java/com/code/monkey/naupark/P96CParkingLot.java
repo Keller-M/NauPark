d6200fabf6a25d321f69e711cf5a85b7d47e2422
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:02 -0700

    created P96CParkingLot.java

---
+++ app/src/main/java/com/code/monkey/naupark/P96CParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P96CParkingLot extends ParkingLot 
+ { 
+
+    public P96CParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
