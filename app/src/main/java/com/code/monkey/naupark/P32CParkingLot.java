fa89283f6d8f43c135ae40307e9ce5687af02203
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:02 -0700

    created P32CParkingLot.java

---
+++ app/src/main/java/com/code/monkey/naupark/P32CParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P32CParkingLot extends ParkingLot 
+ { 
+
+    public P32CParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
