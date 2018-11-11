0ecdeb5ebbe5a9bf16b9a40e93ab44545bad0ebf
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:03 -0700

    created P10ParkingLot.java

---
+++ app/src/main/java/com/code/monkey/naupark/P10ParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P10ParkingLot extends ParkingLot 
+ { 
+
+    public P10ParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
