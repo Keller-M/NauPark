bf2c68adbc79a45ecd9c28f470fcdf7b833abaf0
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:03 -0700

    created P2ParkingLot.java

---
+++ app/src/main/java/com/code/monkey/naupark/P2ParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P2ParkingLot extends ParkingLot 
+ { 
+
+    public P2ParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
