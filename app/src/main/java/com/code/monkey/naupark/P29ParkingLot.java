17e2724d03366f64ec24f1d9c198338722385ab6
Bob Costas <finkzane@gmail.com>
2018-11-10 18:12:02 -0700

    created P29ParkingLot.java

---
+++ app/src/main/java/com/code/monkey/naupark/P29ParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+import java.util.ArrayList;
+public class P29ParkingLot extends ParkingLot 
+ { 
+
+    public P29ParkingLot( ArrayList<ParkingMode> mode, String name ) 
+    {
+        super( mode, name );
+    }
+}
