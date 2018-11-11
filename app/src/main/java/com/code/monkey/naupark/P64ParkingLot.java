061fc58c84c76f6ebe5ebb6ec07b03b75584e95b
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-10 18:44:18 -0700

    added P64ParkingLot

---
+++ app/src/main/java/com/code/monkey/naupark/P64ParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+
+import java.util.ArrayList;
+
+class P64ParkingLot extends ParkingLot {
+    public P64ParkingLot(ArrayList<ParkingMode> mode, String name )
+    {
+        super( mode, name );
+    }
+}
