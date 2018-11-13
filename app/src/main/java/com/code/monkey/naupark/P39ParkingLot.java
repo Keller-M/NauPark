7ccbbb7748d2f1050df75e3ab8063ad4ec92016a
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-12 23:16:44 -0700

    created P39ParkingLot

---
+++ app/src/main/java/com/code/monkey/naupark/P39ParkingLot.java
@@ -0,0 +1,11 @@
+package com.code.monkey.naupark;
+
+import java.util.ArrayList;
+
+class P39ParkingLot extends ParkingLot
+{
+    public P39ParkingLot(ArrayList<ParkingMode> mode, String name )
+    {
+        super( mode, name );
+    }
+}
