3007e00bc721fa6c379fa386f4c46da69a294102
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-12 23:23:56 -0700

    added P8Parkinglot

---
+++ app/src/main/java/com/code/monkey/naupark/P8ParkingLot.java
@@ -0,0 +1,10 @@
+package com.code.monkey.naupark;
+
+import java.util.ArrayList;
+
+class P8ParkingLot extends ParkingLot {
+    public P8ParkingLot(ArrayList<ParkingMode> modes, String name )
+    {
+        super( modes, name );
+    }
+}
