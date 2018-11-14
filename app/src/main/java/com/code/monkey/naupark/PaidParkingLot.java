b2e9b31b3c1dea1820f0f8ae217f7c68719ea431
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-14 00:52:41 -0700

    created PaidParkingLot class

---
+++ app/src/main/java/com/code/monkey/naupark/PaidParkingLot.java
@@ -0,0 +1,31 @@
+package com.code.monkey.naupark;
+
+import java.util.ArrayList;
+
+public class PaidParkingLot extends ParkingLot
+{
+        public PaidParkingLot(ArrayList<ParkingMode> modes, String name )
+    {
+        super( modes, name );
+    }
+
+    public PaidParkingLot(  ArrayList<ParkingMode> modes, String name, String address )
+    {
+        super( modes, name, address );
+        addModes();
+    }
+
+    public PaidParkingLot( String name, String address )
+    {
+        super( new ArrayList<ParkingMode>(), name, address );
+    }
+
+    /**
+     * Adds the default modes for PaidParkingLot
+     */
+    private void addModes( )
+    {
+        this.addMode( ParkingLot.ParkingMode.PAY_TO_PARK );
+    }
+
+}
