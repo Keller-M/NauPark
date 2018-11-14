a270f969200175a0ba4074e40b53c784299876c0
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-11-14 00:40:19 -0700

    added EmployeeParkingLot class

---
+++ app/src/main/java/com/code/monkey/naupark/EmployeeParkingLot.java
@@ -0,0 +1,30 @@
+package com.code.monkey.naupark;
+
+import java.util.ArrayList;
+
+public class EmployeeParkingLot extends ParkingLot
+{
+
+    public EmployeeParkingLot(ArrayList<ParkingMode> modes, String name )
+    {
+        super( modes, name );
+    }
+
+    public EmployeeParkingLot(  ArrayList<ParkingMode> modes, String name, String address )
+    {
+        super( modes, name, address );
+        addModes();
+    }
+
+    /**
+     * Adds the default modes for EmployeeParkingLot
+     */
+    private void addModes( )
+    {
+        this.addMode( ParkingLot.ParkingMode.EMPLOYEE_ONLY );
+        this.addMode( ParkingLot.ParkingMode.AFTERNOON_ON_WEEKDAYS );
+        this.addMode( ParkingLot.ParkingMode.ONLY_ON_WEEKDAYS );
+    }
+
+
+}
