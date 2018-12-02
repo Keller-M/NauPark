713f8c85300ee8c647941c0d102834088f4d2b18
BobCostas <30279553+BobCostas@users.noreply.github.com>
2018-12-02 10:35:40 -0700

    added constructor header

--- app/src/main/java/com/code/monkey/naupark/ParkingPassFactory.java
+++ app/src/main/java/com/code/monkey/naupark/ParkingPassFactory.java
@@ -3,4 +3,17 @@ package com.code.monkey.naupark;
 public class ParkingPassFactory
 {
 
+    /**
+     * Creates the appropriate parking depending upon the type,
+     * passed in.
+     * @param code The string code of Parking Pass to create
+     * @return The parking pass that was created, or null if the code passed
+     *          in does not exist
+     */
+    public ParkingPass createParkingPass( ParkingPassCode code )
+    {
+
+        return null;
+    }
+
 }
