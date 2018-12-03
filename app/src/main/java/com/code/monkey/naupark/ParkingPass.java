4336b9b96fff92842c749647e8b1da54740599d0
zwfink <finkzane@gmail.com>
2018-12-02 17:48:21 -0700

    removed attemptPark method

--- app/src/main/java/com/code/monkey/naupark/ParkingPass.java
+++ app/src/main/java/com/code/monkey/naupark/ParkingPass.java
@@ -27,43 +27,5 @@ abstract public class ParkingPass
      * @return
      */
 
-    public boolean attemptPark(ParkingLot lot, User user, int day, int hours, int minutes)
-    {
-        String lotName = lot.getName();
 
-
-        String parkingPass = user.getParkingPass();
-
-        String query = "SELECT business_hours, after_hours, weekend_hours " +
-                       "FROM lot WHERE " + lotName + "EQUALS lot_id AND " +
-                        parkingPass + "EQUALS pass_id";
-
-        
-        ResultSet rs = null;
-
-
-        return lots.contains( lot );
-    }
-
-
-    /**
-     * Attempts to park in lot, calculates the current day of the week
-     * and time of day.
-     * @param lot Lot to attempt to park in.
-     * @return boolean true/false if user can park now
-     */
-    public boolean attemptParkNow( ParkingLot lot, User user )
-    {
-        int hours   = 0;
-        int minutes = 0;
-        int day     = 0;
-
-        Calendar calendar = Calendar.getInstance();
-
-        hours   = calendar.get( Calendar.HOUR );
-        minutes = calendar.get( Calendar.MINUTE );
-        day     = calendar.get( Calendar.DAY_OF_WEEK );
-
-        return attemptPark( lot, user, day, hours, minutes );
-    }
 }
